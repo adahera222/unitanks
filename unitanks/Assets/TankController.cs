20338c0f715f589ff6687f410b4734b81253b9c4
Boris <boris@2nova.ru>
2012-10-20 16:41:26 +0400

    tank rotation

--- unitanks/Assets/TankController.cs
+++ unitanks/Assets/TankController.cs
@@ -4,36 +4,40 @@ using System.Collections;
 public class TankController : MonoBehaviour {
 	
 	public float speed = 20;
+	public Quaternion initialRotation;
 	// Use this for initialization
 	void Start () {
-	
+		initialRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		rigidbody.velocity = Vector3.zero;
-		if (Input.GetKey(KeyCode.RightArrow)){
+		
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.rotation = initialRotation;
 			rigidbody.AddForce(transform.right * speed, ForceMode.Impulse);
-			//transform.rotation = initialRot;
-			
-	
+			transform.Rotate(new Vector3(0, 180, 0));
 		}
-			if (Input.GetKey(KeyCode.LeftArrow)){
+		
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.rotation = initialRotation;
 			rigidbody.AddForce(-transform.right * speed, ForceMode.Impulse);
-			//transform.rotation = initialRot;
-			
-	
+			transform.Rotate(new Vector3(0, 0, 0));
 		}
-			if (Input.GetKey(KeyCode.DownArrow)){
+		
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			transform.rotation = initialRotation;
 			rigidbody.AddForce(-transform.forward * speed, ForceMode.Impulse);
-			//transform.rotation = initialRot;
-			
-	
+			transform.Rotate(new Vector3(0, -90, 0));
 		}
-			if (Input.GetKey(KeyCode.UpArrow)){
+		
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			transform.rotation = initialRotation;
 			rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
-			//transform.rotation = initialRot;
+			transform.Rotate(new Vector3(0, 90, 0));
 		}
+		
 	}
 }
