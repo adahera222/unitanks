c06a640a27c69044785c90615821bc3e8641248d
master <405112@ro.ru>
2012-10-20 15:38:36 +0400

    add drive to tank

--- unitanks/Assets/TankController.cs
+++ unitanks/Assets/TankController.cs
@@ -12,27 +12,28 @@ public class TankController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.RightArrow)){
-			rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+			rigidbody.AddForce(transform.right * speed, ForceMode.Impulse);
 			//transform.rotation = initialRot;
 			
 	
-	}
+		}
 			if (Input.GetKey(KeyCode.LeftArrow)){
-			rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+			rigidbody.AddForce(-transform.right * speed, ForceMode.Impulse);
 			//transform.rotation = initialRot;
 			
 	
-	}
+		}
 			if (Input.GetKey(KeyCode.DownArrow)){
-			rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
+			rigidbody.AddForce(-transform.forward * speed, ForceMode.Impulse);
 			//transform.rotation = initialRot;
 			
 	
-	}
+		}
 			if (Input.GetKey(KeyCode.UpArrow)){
 			rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
 			//transform.rotation = initialRot;
 			
 	
+		}
 	}
 }
